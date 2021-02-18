533d0b3c11ab7321b8d8ed12106cf8eb17dd1367
SUZUKIYUI <ec2-user@ip-172-31-76-132.ec2.internal>
2021-02-18 07:58:41 +0000

    [Finish]3-3

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,26 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @blog=Blog.new
+  end
+
+  def create
+    blog=Blog.new(blog_params)
+    blog.save
+    redirect_to blogth_path
+  end
+
+  def edit
+  end
+  
+# --------------ここから下には書いちゃダメ！！-------------------
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+end
